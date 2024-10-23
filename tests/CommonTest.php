0a7d2efa044c1393c39506978a65de7a0e346c99
Anton Smirnov <anton.smirnov@dreamapply.com>
2024-10-23 22:43:28 +0300

    Test unserializable class

--- tests/CommonTest.php
+++ tests/CommonTest.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Arokettu\IP\Doctrine\Tests;
 
+use Arokettu\IP\Doctrine\IPAddressType;
 use Arokettu\IP\Doctrine\IPv6AddressType;
 use Doctrine\DBAL\Platforms\SQLitePlatform;
+use Doctrine\DBAL\Types\Exception\InvalidType;
 use PHPUnit\Framework\TestCase;
+use stdClass;
 
 class CommonTest extends TestCase
 {
@@ -17,5 +20,20 @@ class CommonTest extends TestCase
 
         self::assertNull($type->convertToDatabaseValue(null, $platform)); // null in
         self::assertNull($type->convertToPHPValue(null, $platform)); // null out
+    }
+
+    public function testWrongClass(): void
+    {
+        $type = new IPAddressType();
+        $platform = new SQLitePlatform();
+
+        $this->expectException(InvalidType::class);
+        $this->expectExceptionMessage(
+            'Could not convert PHP value of type stdClass to type arokettu_ip. ' .
+            'Expected one of the following types: null, string, Arokettu\IP\IPv4Address, Arokettu\IP\IPv6Address.'
+        );
+
+        // non-ip, non-stringable class
+        $type->convertToDatabaseValue(new stdClass(), $platform);
     }
 }
